f5e20dc22edf6d4c164dc595b94f99d94bb32d71
Sreeja0202 <106142975+Sreeja0202@users.noreply.github.com>
2022-09-16 20:20:12 +0530

    updated

---
+++ FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+const routes: Routes = [{path : '', component: ProductListComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
